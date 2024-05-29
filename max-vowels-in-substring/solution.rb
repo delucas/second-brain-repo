b25b164ae386fba0f5c43e794fc126a751d58dc1
Lucas Videla <videla.lucas@gmail.com>
2024-05-29 09:26:55 -0300

    Con comentarios

--- max-vowels-in-substring/solution.rb
+++ max-vowels-in-substring/solution.rb
@@ -3,10 +3,13 @@ def max_vowel_substring_of_length(s, k)
   max_vowels = 0
   vowels_count = 0
   s.chars.each_with_index do |c, i|
+    # si estoy agregando una vocal
     if vowels.include?(c)
       vowels_count += 1
     end
+    # si ya tengo k caracteres
     if i >= k
+      # si el caracter que estoy sacando es una vocal
       if vowels.include?(s[i - k])
         vowels_count -= 1
       end
